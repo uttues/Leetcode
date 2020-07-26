6d4251b6e0eccb76b5a0fa39de443d8aa40af2c1
uttues <458594033@qq.com>
2020-07-26 20:53:04 +0800

    7.26 fixed 剑指offer-03

--- 剑指Offer/03_数组中重复的数.js
+++ 剑指Offer/03_数组中重复的数.js
@@ -1,15 +1,13 @@
 var findRepeatNumber = function (nums) {
   for (let i = 0, len = nums.length; i < len; i++) {
-    if (nums[i] === nums[nums[i]] && i !== nums[i]) {
-      return nums[i]
-    }
-    if (nums[i] === i) {
-      continue
-    }
+    // 将 nums[i] 放到 nums[nums[i]] 上
     while (nums[i] !== nums[nums[i]]) {
       temp = nums[nums[i]]
       nums[nums[i]] = nums[i]
       nums[i] = temp
+    }
+    if (nums[i] === nums[nums[i]] && i !== nums[i]) {
+      return nums[i]
     }
   }
   return -1
