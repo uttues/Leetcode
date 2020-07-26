b7233ad829161af583d3562dffe6b9756d981109
uttues <458594033@qq.com>
2020-07-26 16:25:51 +0800

    7.26 optimize 268

--- 201-400/268_缺失数字.js
+++ 201-400/268_缺失数字.js
@@ -1,17 +1,20 @@
 var missingNumber = function (nums) {
-  let temp
-  // 遍历数组，目的：下标i的位置上放着数字i
-  for (let i = 0, len = nums.length; i < len; i++) {
-    if (nums[i] === i) continue
-    // 循环中：让nums[i] 放到它该在的地方（nums[nums[i]]===nums[i]）
-    while (nums[i] !== undefined && nums[i] !== i) {
+  let len = nums.length
+
+  // 遍历数组，目的：让  nums[i] = nums[nums[i]]
+  for (let i = 0; i < len; i++) {
+    // nums[i]没有放在正确的位置上
+    while (nums[i] !== nums[nums[i]]) {
       temp = nums[nums[i]]
       nums[nums[i]] = nums[i]
       nums[i] = temp
     }
   }
-  // 数组中必然有某一个数与nums[n] 交换了，那么该数组下标上的数就是undefined，说明该下标值是缺失的
-  let res = nums.findIndex(value => value === undefined)
-  return res === -1 ? nums.length : res
+
+  for (let i = 0; i < len; i++) {
+    if (nums[i] !== i) {
+      return i
+    }
+  }
+  return len
 }
-console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
